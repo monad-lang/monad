677f62598edc58d2043006eb84eac135bd89e572
Anders Christiansen Sørby <anders@sorby.xyz>
2026-04-29 13:27:00 +0200

    feat(llvm-codegen): add reference counting runtime
    C runtime providing memory management primitives for generated
    LLVM IR. All heap objects share a common Header with atomic
    reference counting.
    
    Runtime structures:
    - Header: refcount (atomic), tag, flags
    - Closure: entry point, arity, env_size, flexible env array
    - Constructor: tag, field_count, flexible fields array
    - StringObj: length, flexible data array
    
    Runtime functions:
    - monad_alloc/monad_free: base allocation
    - monad_retain/monad_release: atomic refcounting
    - alloc_closure: create function closures with captured env
    - alloc_constructor: create inductive constructor values
    - alloc_string: create heap-allocated strings
    - monad_print_i64/monad_print_str: I/O primitives
    
    Designed with extension points for future region-based GC:
    the MemoryManager abstraction allows swapping the RC
    implementation for arena/borrow-checking style management.

---
+++ llvm-codegen/src/runtime/mod.rs
@@ -0,0 +1,107 @@
+pub struct RuntimeBuilder;
+
+impl RuntimeBuilder {
+    pub fn c_source() -> &'static str {
+        r#"#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdatomic.h>
+
+typedef struct {
+    int64_t refcount;
+    uint16_t tag;
+    uint16_t flags;
+} Header;
+
+typedef struct {
+    Header header;
+    void* entry;
+    int64_t arity;
+    int64_t env_size;
+    void* env[];
+} Closure;
+
+typedef struct {
+    Header header;
+    int64_t tag;
+    int64_t field_count;
+    void* fields[];
+} Constructor;
+
+typedef struct {
+    Header header;
+    int64_t length;
+    char data[];
+} StringObj;
+
+void* monad_alloc(size_t size) {
+    void* ptr = malloc(size);
+    if (ptr) {
+        Header* h = (Header*)ptr;
+        h->refcount = 1;
+        h->tag = 0;
+        h->flags = 0;
+    }
+    return ptr;
+}
+
+void monad_retain(void* ptr) {
+    if (!ptr) return;
+    Header* h = (Header*)ptr;
+    atomic_fetch_add(&h->refcount, 1);
+}
+
+void monad_release(void* ptr) {
+    if (!ptr) return;
+    Header* h = (Header*)ptr;
+    if (atomic_fetch_sub(&h->refcount, 1) == 1) {
+        free(h);
+    }
+}
+
+Closure* alloc_closure(void* entry, int64_t arity, int64_t env_size) {
+    size_t size = sizeof(Closure) + env_size * sizeof(void*);
+    Closure* c = (Closure*)monad_alloc(size);
+    if (c) {
+        c->header.tag = 1;
+        c->entry = entry;
+        c->arity = arity;
+        c->env_size = env_size;
+    }
+    return c;
+}
+
+Constructor* alloc_constructor(int64_t tag, int64_t field_count) {
+    size_t size = sizeof(Constructor) + field_count * sizeof(void*);
+    Constructor* c = (Constructor*)monad_alloc(size);
+    if (c) {
+        c->header.tag = 2;
+        c->tag = tag;
+        c->field_count = field_count;
+    }
+    return c;
+}
+
+StringObj* alloc_string(char* data, int64_t length) {
+    size_t size = sizeof(StringObj) + length + 1;
+    StringObj* s = (StringObj*)monad_alloc(size);
+    if (s) {
+        s->header.tag = 3;
+        s->length = length;
+        memcpy(s->data, data, length);
+        s->data[length] = '\0';
+    }
+    return s;
+}
+
+void monad_print_i64(int64_t n) {
+    printf("%ld\n", n);
+}
+
+void monad_print_str(char* s) {
+    printf("%s\n", s);
+}
+"#
+    }
+}
